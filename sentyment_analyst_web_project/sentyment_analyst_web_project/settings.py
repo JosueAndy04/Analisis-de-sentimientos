4b7c3b09f830d4e60b687b920d8e2d190366c69b
Anderson Josue <anderjosue456@gmail.com>
2025-07-18 23:57:17 -0500

    refactor: :zap: Mejor requereriments and better setting.py for deploy
    Se reducio la cantidad de requerimientos para backend y frontend ademas se cambio el settings.py para produccion

--- sentyment_analyst_web_project/sentyment_analyst_web_project/settings.py
+++ sentyment_analyst_web_project/sentyment_analyst_web_project/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a@^x&$f!hr_)r^+3_gp@w**43@_=6z@@^vp!fl4)@u201-0av@"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "tu_clave_secreta_segura")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "analisis-de-sentimientos-c9ef.onrender.com").split(",")
 
 
 # Application definition
@@ -37,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'base.apps.BaseConfig',
+    "base.apps.BaseConfig",
 ]
 
 MIDDLEWARE = [
